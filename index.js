fcdcee43403aa6e423de69d2b5fc5f044bc2964d
risper4 <risperwanjikugichia@gmail.com>
2026-03-19 17:39:41 +0300

    function test

--- index.js
+++ index.js
@@ -3,26 +3,39 @@ function calculateTax (amount){
     const taxValue = amount * (10/100)
     return taxValue
 }
+console.log(calculateTax(100))
 
 
 function convertToUpperCase (text){
-    const upperCase = (convertToUpperCase(text))
+    const upperCase = text.toUpperCase()
     return upperCase
 }
+console.log(convertToUpperCase("discount"));
+
 
 
 function findMaximum (num1, num2){
     const largerNum = Math.max(num1, num2)
     return largerNum
 }
+console.log(findMaximum('25','72'));
+
 
 
 function isPalindrome (word){
     const palindrome = word.split("").reverse().join("")
     return word === palindrome
 }
+console.log(isPalindrome("level"));
+console.log(isPalindrome("race"));
 
 
+function calculateDiscountedPrice (originalPrice, discountPercentage){
+    let discount = originalPrice * (discountPercentage/100)
+    priceAfterDiscount = originalPrice + discount
+    return priceAfterDiscount
+}
+console.log(calculateDiscountedPrice(200, 15))
 
 
 
